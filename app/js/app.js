3c7ee5c69d44dbfca397cfbc0ead771462c8e286
Sergey Valitov <serejatoje@gmail.com>
2020-12-03 12:56:23 +0700

    Проекты

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,15 @@
 $(document).ready(function () {
-    console.log("%cCreated by Sergey Valitov, with ❤","color: #000; padding: 5px 0px 1px; border-bottom:2px solid #17a1a5;");
+
+    /* Для устройств с поддержкой touch events */
+
+        let supportsTouch = 'ontouchstart' in window || navigator.msMaxTouchPoints;
+        if (supportsTouch) {
+            $('.project__card').addClass('project__card-touch');
+        }
+
+    /* */
+
+    console.log("%cCreated with love ❤","color: #000; padding: 5px 0px 1px; border-bottom:2px solid #17a1a5;");
 
     $('[type="tel"]').mask('+7 (999) 999-99-99');
 
