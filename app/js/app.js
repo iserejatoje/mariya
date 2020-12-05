c58012e776b4f37cdbf8d91f561ce63390e63732
Sergey Valitov <serejatoje@gmail.com>
2020-12-05 13:21:52 +0700

    Лого

--- app/js/app.js
+++ app/js/app.js
@@ -1,15 +1,11 @@
 $(document).ready(function () {
 
-    /* Для устройств с поддержкой touch events */
-
-        let supportsTouch = 'ontouchstart' in window || navigator.msMaxTouchPoints;
-        if (supportsTouch) {
-            $('.project__card').addClass('project__card-touch');
-        }
-
-    /* */
-
     console.log("%cCreated with love ❤","color: #000; padding: 5px 0px 1px; border-bottom:2px solid #17a1a5;");
+
+    let supportsTouch = 'ontouchstart' in window || navigator.msMaxTouchPoints;
+    if (supportsTouch) {
+        $('.project__card').addClass('project__card-touch');
+    }
 
     $('[type="tel"]').mask('+7 (999) 999-99-99');
 
